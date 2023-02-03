0216e350cd639af6251e06e5d0765410972ffa6f
Temur Zhakibaev <temurzhakibaev@gmail.com>
2023-02-03 16:01:37 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+const countriesElem = document.querySelector(".countries");
+const dropDown = document.querySelector(".dropDown");
+const dropElem = document.querySelector(".drop");
+const region = document.querySelectorAll(".region");
+const country = document.querySelector(".country");
+const search = document.querySelector(".search");
+
+async function getCuontry() {
+  const URL = await fetch("https://restcountries.com/v3.1/all");
+  const res = await URL.json();
+  console.log(res);
+  res.forEach((element) => {
+    showCountry(element);
+  });
+}
+
+getCuontry();
+
+function showCountry(data) {
+  const country = document.createElement("div");
+  country.classList.add("country");
+  country.innerHTML = `
+  <div class="country">
+  <div class="country-img">
+  <img src="${data.flags.png}" alt="" />
+  </div>
+  <div class="country-info">
+  <h5 class="countryName">${data.name.common}</h5>
+  <p><strong>Население: </strong>${data.population}</p>
+  <p class="regionName"><strong>Регион: </strong>${data.region}</p>
+  <p><strong>Столица: </strong>${data.capital}</p>
+  </div>
+  </div>
+
+  `;
+  countriesElem.append(country);
+}
+
+dropDown.addEventListener("click", () => {
+  dropElem.classList.toggle("showDropDown");
+  console.log("fqq");
+});
+
+const regionName = document.getElementsByClassName("regionName");
+const countryName = document.getElementsByClassName("countryName");
+console.log(regionName);
+region.forEach((element) => {
+  element.addEventListener("click", () => {
+    console.log(element);
+    Array.from(regionName).forEach((elem) => {
+      console.log(elem.innerText);
+      if (
+        elem.innerText.includes(element.innerText) ||
+        element.innerText == "All"
+      ) {
+        elem.parentElement.parentElement.style.display = "grid";
+      } else {
+        elem.parentElement.parentElement.style.display = "none";
+      }
+    });
+  });
+});
+
+search.addEventListener("input", () => {
+  Array.from(countryName).forEach((elem) => {
+    console.log(elem.innerText);
+    if (elem.innerText.toLowerCase().includes(search.value.toLowerCase())) {
+      elem.parentElement.parentElement.style.display = "grid";
+    } else {
+      elem.parentElement.parentElement.style.display = "none";
+    }
+  });
+});
